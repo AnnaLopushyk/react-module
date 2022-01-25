a1894882fff11cebc64546e1f2a9ba0273db0dca
AnnaLopushyk <anna.lopushyk@gmail.com>
2022-01-25 16:05:46 +0200

    cw2-full

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
+import Form from "./components/Form/Form";
+import {userService} from "./services/user.service";
+import Users from "./components/Users/Users";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [usersFilter, setUsersFilter] = useState([]);
+
+useEffect(()=> {
+  userService.getAll().then(value => {
+    setUsers([...value])
+        setUsersFilter([...value])
+  })
+}, [])
+
+  const getFilter = (data)=> {
+     let filterArr = [...users]
+
+
+      if(data.name) {
+          filterArr = filterArr.filter(user=> user.name.toLowerCase().includes(data.name.toLowerCase()))
+      }
+      if(data.username) {
+          filterArr = filterArr.filter(user=> user.username.toLowerCase().includes(data.username.toLowerCase()))
+      }
+      if(data.email) {
+          filterArr = filterArr.filter(user=> user.email.toLowerCase().includes(data.email.toLowerCase()))
+      }
+      setUsersFilter(filterArr)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Form getFilter={getFilter}/>
+             <hr/>
+            <Users users={usersFilter}/>
     </div>
   );
 }
