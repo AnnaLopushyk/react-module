31ae7b07e13b679d0f3226f8cd7a1bc77abcb2cc
AnnaLopushyk <anna.lopushyk@gmail.com>
2022-01-27 20:48:01 +0200

    hw-3

--- src/services/users.service.js
+++
@@ -1,7 +0,0 @@
-import {urls} from '../configs/urls';
-import {axiosService} from './axios.service';
-
-export const usersService = {
-    getAll:()=>axiosService.get(urls.users).then(value => value.data),
-    getById:(id)=>axiosService.get(`${urls.users}/${id}`).then(value => value.data)
-}
